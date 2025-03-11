5d2a8a193b22643b1ca813149d7dc3eabf79a467
hanjiwon1108 <jwjwhan1108@gmail.com>
2025-03-12 01:19:57 +0900

    feat: fetch를 사용해 file 정보 get으로 불러오기

--- src/shared/stores/useDocument.ts
+++ src/shared/stores/useDocument.ts
@@ -1,7 +1,6 @@
 import { create } from "zustand";
 import deepDiff from "deep-diff";
 import { parseHtmlToArray } from "@/utils/parseHtmlToArray";
-import axios from "axios";
 
 interface RefineState {
   preDocument: string[];
@@ -28,6 +27,7 @@ interface FileItem {
   updated_at: string;
   hashed_id: string;
 }
+
 interface FileStore {
   files: FileItem[];
   fetchFiles: () => Promise<void>;
@@ -37,8 +37,29 @@ export const useFileStore = create<FileStore>((set) => ({
   files: [],
   fetchFiles: async () => {
     try {
-      const response = await axios.get<FileItem[]>("/files/list");
-      set({ files: response.data ?? [] });
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/files/list?skip=0&limit=100`,
+        {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+          credentials: "include",
+        }
+      );
+
+      if (!response.ok)
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      const rawData = await response.json();
+      const data = rawData.map((file: FileItem) => ({
+        ...file,
+        updated_at: new Date(file.updated_at.replace(" ", "T")).toISOString(),
+      }));
+
+      set((state) =>
+        state.files.length === data.length &&
+        state.files.every((file, i) => file.hashed_id === data[i].hashed_id)
+          ? state
+          : { files: data }
+      );
     } catch (error) {
       console.error("Failed to fetch files:", error);
       set({ files: [] });
