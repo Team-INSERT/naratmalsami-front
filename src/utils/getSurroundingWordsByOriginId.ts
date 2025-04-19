aa32434f5167b4abd99e429bc6948c3af1606fcc
galbimandu <galbimandu1126@gmail.com>
2025-04-20 04:11:16 +0900

    add : getSurrroundingWordsByOriginId에서 josa를 고려한 연산을 할 수 있도록 하는 기능을 추가함

--- src/utils/getSurroundingWordsByOriginId.ts
+++ src/utils/getSurroundingWordsByOriginId.ts
@@ -1,7 +1,21 @@
-export default function getSurroundingWordsByOriginId(targetId : string, originId : string, count = 3) {
-  const editors = document.querySelectorAll(".ck-content");
-  if (editors.length === 0) return { before: [], after: [] };
-  const editor = editors[0];
+import translateJosa from "./josa/translateJosa";
+
+export default function getSurroundingWordsByOriginId(
+  targetId: string,
+  originId: string,
+  refinedWord: string,
+  considerJosa: boolean = true,
+  count = 3
+): {
+  before: string[];
+  after: string[];
+  josaDetail?: {
+    beforeJosa: string | null;
+    afterJosa: string | null;
+  };
+} {
+  const editor = document.querySelectorAll(".ck-content")[0];
+
   const p = editor.querySelector(`p[data-unique="${targetId}"]`);
 
   if (!p) return { before: [], after: [] };
@@ -10,17 +24,54 @@ export default function getSurroundingWordsByOriginId(targetId : string, originI
   const span = clone.querySelector(`span[originid="${originId}"]`);
   if (!span) return { before: [], after: [] };
 
-  const MARKER = '<<<>>>';
+  const MARKER = "<<<>>>";
   span.replaceWith(MARKER);
   const textContent = clone.textContent;
   if (!textContent) return { before: [], after: [] };
 
+  // 조사 찾기
+  let afterJosa: string | null = null;
+  let beforeJosa: string | null = null;
+
+  if (considerJosa) {
+    const markerIndex = textContent.indexOf(MARKER);
+    if (markerIndex === -1) return { before: [], after: [] };
+
+    const startIndex = markerIndex + MARKER.length;
+    const nextSpaceIndex = textContent.substring(startIndex).indexOf(" ");
+
+    if (nextSpaceIndex > -1) {
+      afterJosa = textContent.substring(
+        startIndex,
+        startIndex + nextSpaceIndex
+      );
+    } else {
+      afterJosa = textContent.substring(startIndex);
+    }
+    if (afterJosa === "") afterJosa = null;
+    if (afterJosa) {
+      beforeJosa = translateJosa(refinedWord as string, afterJosa);
+    }
+  }
+
   const tokens = textContent.replace(MARKER, ` ${MARKER} `).trim().split(/\s+/);
   const idx = tokens.indexOf(MARKER);
   if (idx < 0) return { before: [], after: [] };
+  const after = tokens.slice(idx + 1, idx + 1 + count);
+
+  if (considerJosa) {
+    // 문장 뒤에 변경해야할 조사가 존재한다면 after에서 조사 원소를 삭제
+    if (!!afterJosa && after[0] === afterJosa) {
+      after.shift();
+    }
+  }
 
   return {
     before: tokens.slice(Math.max(0, idx - count), idx),
-    after: tokens.slice(idx + 1, idx + 1 + count),
+    after: after,
+    josaDetail: {
+      beforeJosa: afterJosa,
+      afterJosa: beforeJosa,
+    },
   };
 }
